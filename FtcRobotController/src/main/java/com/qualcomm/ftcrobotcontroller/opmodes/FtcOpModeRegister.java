15d35cb8260ea12a58ab5ce6c0f355ede3960042
umer936 <umer936@gmail.com>
2015-10-26 20:11:03 -0500

    Sorted stuff. Arcade vs Tank.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,12 +55,19 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
+//    manager.register("NullOp", NullOp.class);
 
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register("PushBotAuto", PushBotAuto.class);
+    //manager.register("PushBotManual", PushBotManual.class);
+
+
+
+    manager.register("TankTeleOp", TankTeleOp.class);
+
+
+
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankTeleOp.java
@@ -0,0 +1,91 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * Created by umer936 on 10/26/15.
+ */
+public class TankTeleOp extends OpMode {
+
+    // Initialize Motors
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+    public TankTeleOp() {
+
+    }
+
+
+    @Override
+    public void init() {
+	    motorRight = hardwareMap.dcMotor.get("motor_2");
+        motorLeft = hardwareMap.dcMotor.get("motor_1");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+    @Override
+    public void loop() {
+
+        float right = -gamepad1.left_stick_y;
+        float left = gamepad1.right_stick_y;
+
+        // clip the right/left values so that the values never exceed +/- 1
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float)scaleInput(right);
+        left =  (float)scaleInput(left);
+
+        // write the values to the motors
+        motorRight.setPower(right);
+        motorLeft.setPower(left);
+
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("",  "left  pwr: " + String.format("%.2f", left));
+        telemetry.addData("", "right pwr: " + String.format("%.2f", right));
+
+    }
+
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+     */
+    @Override
+    public void stop() {
+
+    }
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        return dScale;
+    }
+}
