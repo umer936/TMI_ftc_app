771834bdb2084fa5611888755739ab9c3031487e
parker836 <parkeroberg98@gmail.com>
2016-01-16 11:40:35 -0600

    Added Drive Code
    Parker added mapping for controllers to variables and started setting up
    drive code. Drive code is completed for left and right drive,

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Switchback.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Switchback.java
@@ -39,7 +39,7 @@ public class Switchback extends OpMode {
 
     @Override
     public void init(){
-        // We dont know which is really which yet
+        // Define Motor mapping to variables
         motorRight = hardwareMap.dcMotor.get("motor_1");
         motorLeft = hardwareMap.dcMotor.get("motor_2");
         motor118L = hardwareMap.dcMotor.get("motor_3");
@@ -48,31 +48,100 @@ public class Switchback extends OpMode {
         motorJog = hardwareMap.dcMotor.get("motor_6");
         motorLift = hardwareMap.dcMotor.get("motor_7");
 
+        //Define Servo mapping to variables
         hookLeft = hardwareMap.servo.get("servo_1");
         hookRight = hardwareMap.servo.get("servo_2");
         bucket = hardwareMap.servo.get("servo_3");
         intakeHeight = hardwareMap.servo.get("servo_4");
         shelterPeople = hardwareMap.servo.get("servo_5");
+
+
+
     }
 
     @Override
     public void loop() {
-        float right = gamepad1.left_stick_y;
-        float left = gamepad1.right_stick_y;
+        //Map controllers to variables
+        float driverRight = gamepad1.left_stick_y;
+        float driverLeft = gamepad1.right_stick_y;
+        float operatorRight = gamepad2.right_stick_y;
+        float operatorLeft = gamepad2.left_stick_y;
+        boolean driverTriggerRight = gamepad1.right_trigger;
+        boolean driverTriggerLeft = gamepad1.left_trigger;
+        boolean operatorX = gamepad2.x;
+        boolean operatorY = gamepad2.y;
+        boolean operatorA = gamepad2.a;
+        boolean operatorB = gamepad2.b;
+        boolean operatorTriggerRight = gamepad2.right_trigger;
+        boolean operatorTriggerLeft = gamepad2.left_trigger;
+        boolean operatorBumperRight = gamepad2.right_bumper;
+        boolean operatorBumperLeft = gamepad2.left_bumber;
+
+        float MultiplierIntake = 1;
+        float Multiplier118 = 1;
+
+        float frontHooksOriginal;
+        float frontHooksDeployed;
+
+        float bucketOriginal;
+        float bucketDeployed;
+        
 
         // clip the right/left values so that the values never exceed +/- 1
-        right = Range.clip(right, -1, 1);
-        left = Range.clip(left, -1, 1);
+        driverRight = Range.clip(driverRight, -1, 1);
+        driverLeft = Range.clip(driverLeft, -1, 1);
+        operatorRight = Range.clip(operatorRight, -1, 1);
+        operatorLeft = Range.clip(operatorLeft, -1, 1);
+
+
 
 
         // scale the joystick value to make it easier to control
         // the robot more precisely at slower speeds.
-        right = (float)scaleInput(right);
-        left = (float)scaleInput(left);
+        driverRight = (float)scaleInput(driverRight);
+        driverLeft = (float)scaleInput(driverLeft);
+        operatorRight = (float)scaleInput(operatorRight);
+        operatorLeft = (float)scaleInput(operatorLeft);
+
 
         // write the values to the motors
-        motorRight.setPower(left);
-        motorLeft.setPower(right);
+        motorRight.setPower(driverLeft);
+        motorLeft.setPower(driverRight);
+
+        motorLift.setPower(operatorLeft);
+
+        if(operatorTriggerLeft)
+        {
+            motor118R.setPower(-1*Multiplier118);
+            motor118L.setPower(-1*Multiplier118);
+        }
+        else if(operatorTriggerRight)
+        {
+            motor118R.setPower(1*Multiplier118);
+            motor118L.setPower(1*Multiplier118);
+        }
+        else
+        {
+            motor118R.setPower(0);
+            motor118L.setPower(0);
+        }
+
+
+        if(operatorBumperLeft)
+        {
+            motorIntake.setPower(-1*MultiplierIntake);
+        }
+        else if(operatorBumperRightr)
+        {
+            motorIntake.setPower(1*MultiplierIntake);
+        }
+        else
+        {
+            motorIntake.setPower(0);
+        }
+
+
+
     }
 
 
